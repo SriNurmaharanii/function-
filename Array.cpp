74ffa07f0e9c1a9e7050af023042d1930dc5be01
SriNurmaharanii <113900546+SriNurmaharanii@users.noreply.github.com>
2022-10-10 12:57:21 +0700

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+int angka [] = {3,5,6,8,12};
+cout <<"angka"<< angka[4]<<endl; 
+char huruf[] = {'x','y','z'};
+printf ("huruf %c",huruf [1]);
+}
---
+++ function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+void halo(string nama){
+  cout << "Halo"<< nama << endl;
+  }
+  
+  int main (){
+      halo ("kamal");
+      halo ("mega");
+      halo ("tegar");
+   
+   }
